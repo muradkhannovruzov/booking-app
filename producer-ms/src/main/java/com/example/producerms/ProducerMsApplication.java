243efa19d7ed3648e9b267a60848317e4b98a763
muradxan <muradkhannovruzov@gmail.com>
2023-10-22 17:48:32 +0400

    Created produce service

---
+++ producer-ms/src/main/java/com/example/producerms/ProducerMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.producerms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProducerMsApplication.class, args);
+    }
+
+}
---
+++ producer-ms/src/test/java/com/example/producerms/ProducerMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.producerms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
