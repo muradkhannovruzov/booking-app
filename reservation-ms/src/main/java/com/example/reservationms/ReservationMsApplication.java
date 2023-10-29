84f74eef3d3ff98089c7df821e816cc2a3982fe0
muradxan <muradkhannovruzov@gmail.com>
2023-10-29 23:40:23 +0400

    Created reservation-ms

---
+++ reservation-ms/src/main/java/com/example/reservationms/ReservationMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.reservationms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservationMsApplication.class, args);
+    }
+
+}
---
+++ reservation-ms/src/test/java/com/example/reservationms/ReservationMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reservationms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
